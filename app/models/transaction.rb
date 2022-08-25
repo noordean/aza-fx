3abf5e16ce0619f80ca323b8ad200a5f6e1e8153
Nurudeen Ibrahim <nurudeen@Nurudeens-MacBook-Pro.local>
2022-08-25 21:22:45 +0100

    Add validations to transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,10 @@
 class Transaction < ApplicationRecord
+  VALID_CURRENCIES = %w[USD NGN AED EUR GBP GHS JPY KES MAD UGX XOF ZAR].freeze
+
   belongs_to :customer
+
+  validates :input_amount,  numericality: { greater_than_or_equal_to: 0 }
+  validates :output_amount,  numericality: { greater_than_or_equal_to: 0 }
+  validates :input_currency, inclusion: { in: VALID_CURRENCIES, allow_nil: false }
+  validates :output_currency, inclusion: { in: VALID_CURRENCIES, allow_nil: false }
 end
